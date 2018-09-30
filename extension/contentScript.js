3527906afab11c523b7293ce691ff57470bf56fe
Martin R-L <martin.rosenlidholm@gmail.com>
2018-09-30 11:58:21 +0200

    add cancelled

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,20 +1,40 @@
 window.onload = function () {
     setInterval(
-        function () { 
-                $(".estimated-time").each( 
-                    function(index) { 
-                        var key = $(this).parent().parent().text().hashCode();
-                        var delay = {};
-                        delay[key] = $(this).parent().parent().html();
-                        chrome.storage.sync.set(
-                            { delay }, 
-                            function() {
-                                console.log("La till en försening/inställt.");
-                            })  
-                    } )
+        function () {
+            //$(".estimated-time").each(addToSyncStorage($(this)));
+            $(".estimated-time").each(function (index) {
+                var key = $(this).parent().parent().text().hashCode();
+                var value = $(this).parent().parent().html();
+                var delay = {};
+                delay[key] = value;
+                chrome.storage.sync.set({ delay }, function () {
+                    console.log("La till en försening/inställt: \nKey (hash): " + key + "\nValue (html): " + value + "\n");
+                });
+            });
+
+            //$(".cancelled").each(addToSyncStorage($(this)));
+            $(".cancelled").each(function (index) {
+                var key = $(this).parent().parent().text().hashCode();
+                var delay = {};
+                var value = $(this).parent().parent().html();
+                delay[key] = value;
+                chrome.storage.sync.set({ delay }, function () {
+                    console.log("La till en försening/inställt: \nKey (hash): " + key + "\nValue (html): " + value + "\n");
+                });
+            });
         }, 
         500);
 }
+
+var addToSyncStorage = function (node) {
+    console.log("Nod: " + node.text() + "\n");
+    var key = node.parent().parent().text().hashCode();
+    var delay = {};
+    delay[key] = node.parent().parent().html();
+    chrome.storage.sync.set({ delay }, function () {
+        console.log("La till en försening/inställt: \nKey (hash): " + key + "\nValue (html): " + node.parent().parent().html() + "\n");
+    });
+};
 
 String.prototype.hashCode = function() {
     var hash = 0, i, chr;
